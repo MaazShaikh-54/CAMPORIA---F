e9dc6ac801bf0284f44301c47944dfc72019755e
Maaz <mohdmaaz.612054.it@mhssce.ac.in>
2025-03-08 20:32:46 +0530

    Update details page

--- src/pages/detail.jsx
+++ src/pages/detail.jsx
@@ -1,25 +1,13 @@
-import { useParams } from "react-router-dom";
 import Navbar from '../components/Navbar/navbar.jsx'
+import Details from '../components/CardDetail/cardDetail.jsx'
 import Footer from '../components/Footer/footer.jsx'
-import campsites from "../components/Search/campsiteData"
 
 const Detail = () => {
-  const { id } = useParams(); // Get ID from URL
-  const campsite = campsites.find((camp) => camp.id === Number(id)); // Find campsite by ID
-
-  if (!campsite) {
-    return <h2>Destination Not Found!</h2>;
-  }
 
   return (
     <>
     <Navbar/>
-    <div className="detail-container">
-      <img src={campsite.imageUrl} alt={campsite.campsiteName} className="detail-image" />
-      <h2>{campsite.campsiteName}</h2>
-      <p>Price: {campsite.price}</p>
-      <p>Description: {campsite.description}</p>
-    </div>
+    <Details/>    
     <Footer/>
     </>
   );
