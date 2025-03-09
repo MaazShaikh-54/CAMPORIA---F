35f6ec86c29a59a0f54a2000860af48cb345c524
Maaz <mohdmaaz.612054.it@mhssce.ac.in>
2025-03-10 05:01:40 +0530

    feat payment

---
+++ src/components/Payment/payment.jsx
@@ -0,0 +1,17 @@
+import './payment.css'
+import Navbar from '../Navbar/navbar.jsx';
+import Footer from '../Footer/footer.jsx';
+
+const Payment = () => {
+    return (
+        <>
+            <div className='payment'>
+                <Navbar />
+                <h1>Payment</h1>
+                <Footer />
+            </div>
+        </>
+    );
+}
+
+export default Payment;
