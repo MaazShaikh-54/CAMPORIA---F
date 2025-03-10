8e3eab1b991ef9ee6f93e6e0d9b60e023c58568c
Maaz <mohdmaaz.612054.it@mhssce.ac.in>
2025-03-10 20:39:36 +0530

    fixed route

--- src/components/Payment/payment.jsx
+++ src/components/Payment/payment.jsx
@@ -1,13 +1,24 @@
 import './payment.css'
 import Navbar from '../Navbar/navbar.jsx';
 import Footer from '../Footer/footer.jsx';
+import Count from '../Count/count.jsx'
+import campsites from '../Search/campsiteData.js';
+import { useParams } from 'react-router-dom';
 
 const Payment = () => {
+    const { id } = useParams();
+    const campsite = campsites.find((camp) => camp.id === Number(id));
+
+    if (!campsite) {
+        return <h2>Destination Not Found!</h2>;
+    }
+
     return (
         <>
             <div className='payment'>
                 <Navbar />
                 <h1>Payment</h1>
+                <Count/>
                 <Footer />
             </div>
         </>
