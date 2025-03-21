a345a26bf949b67cec884d186db9ad24b32a6e95
Maaz <mohdmaaz.612054.it@mhssce.ac.in>
2025-03-21 06:49:29 +0530

    Added Prop

--- src/components/Button/button.jsx
+++ src/components/Button/button.jsx
@@ -1,15 +1,16 @@
 import './button.css';
 import propsTypes from 'prop-types';
 
-const Button = ({ text, onClick, type, value }) => {
-  return <button onClick={onClick} type={type} value={value} className='btn'>{text}</button>;
+const Button = ({ text, onClick, type, value, className }) => {
+  return <button onClick={onClick} type={type} value={value} className={className}>{text}</button>;
 }
 
 Button.propTypes = {
   text: propsTypes.string.isRequired,
   onClick: propsTypes.func.isRequired,
   type: propsTypes.string,
-  value: propsTypes.string
+  value: propsTypes.string,
+  className: propsTypes.string
 }
 
 export default Button
