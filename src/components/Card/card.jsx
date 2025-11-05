e87f22c1bcdedee30e1a12a7827a6e80c1137931
M. MAAZ SHAIKH <109799361+MaazShaikh-54@users.noreply.github.com>
2025-11-05 18:01:10 +0530

    Implement favorite toggle in CampsiteCard
    Added favorite functionality to CampsiteCard component.

--- src/components/Card/card.jsx
+++ src/components/Card/card.jsx
@@ -2,24 +2,30 @@ import './card.css';
 import PropTypes from "prop-types";
 import { useNavigate } from "react-router-dom";
 import { Heart, Star, Trophy } from 'lucide-react';
+import { useState } from 'react';
 
 const CampsiteCard = ({ id, imageUrl, campsiteName, price }) => {
+  const [isFavorite, setIsFavorite] = useState(false);
+  const toggleFavorite = () => {
+    setIsFavorite(!isFavorite);
+  };
   const navigate = useNavigate();
 
   return (
     <>
-      <div className="campsite-card" onClick={() => navigate(`/detail/${id}`)}>
+      <div className="campsite-card" >
         <img src={imageUrl} alt={campsiteName} className="campsite-image" />
         <p className='campsite-info campsite-badge'><Trophy className='lucide-icon' />Popular</p>
-        <Heart className='heart-icon' fill='#00000078' strokeWidth={1.25} />
+        <Heart className='heart-icon' color={isFavorite ? "#ffffff82" : "#ffffffb3"} onClick={toggleFavorite} fill={isFavorite ? "#ff0000ff" : '#00000078'} strokeWidth={1.25} />
         <div>
           <div className="campsite-info">
             <h3 className="campsite-name">{campsiteName}</h3>
-            <div className="review-star"><Star fill='#000' strokeWidth={1} className='lucide-icon' />{4.5}{" (23)"}</div>
+            <div className="review-star"><Star fill='#000' strokeWidth={0} className='lucide-icon' />{4.5}</div>
           </div>
           <div className="campsite-footer">
             <p className='campsite-amenities'>WiFi • Laundry • Shower</p>
             <p className="campsite-price"><span className='campsite-price-number'>₹{price.toLocaleString('en-US')}</span> <span className='campsite-price-text'>for a night</span></p>
+            <button onClick={() => navigate(`/detail/${id}`)} className='campsite-btn-book'>Book</button>
           </div>
         </div>
       </div>
