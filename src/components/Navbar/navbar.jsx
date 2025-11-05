9c77926fadb0a7ee4d7111c9e3ae0af422f14fb0
M. MAAZ SHAIKH <109799361+MaazShaikh-54@users.noreply.github.com>
2025-11-05 23:36:59 +0530

    Update Navbar with Tent icon and menu structure
    Refactor Navbar component to include Tent icon and restructure menu.

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -2,12 +2,13 @@ import './navbar.css'
 import { useState } from 'react';
 import Button from '../Button/button'
 import { Link } from "react-router-dom";
+import { Tent } from 'lucide-react';
 
 const Navbar = () => {
     const [menu, setMenu] = useState(false);
 
     const toggleMenu = () => {
-        setMenu(true);                
+        setMenu(true);
     }
 
     return (
@@ -15,31 +16,36 @@ const Navbar = () => {
             <header>
                 <nav className="navbar-container">
                     <div className="logo" onClick={() => { <Link to="/" /> }}>
-                        <img className='nav-logo' src="/assets/camporia-nav-logo-b.png" alt="Logo" />
+                        <Tent size={45} color="#000" strokeWidth={1.3} />
                         <h1 className="logo-txt">Camporia</h1>
                     </div>
-                    <ul>
-                        <li className="li-items"><Link to="/" id='link'>Home</Link></li>
-                        <li className="li-items"><Link to="/blog" id='link'>Blog</Link></li>
-                        <li className="li-items"><Link to="/aboutus" id='link'>About Us</Link></li>
-                        <li className="li-items"><Link to="/help" id='link'>Help</Link></li>
-                        <li className="li-items"><Link to="/bookings" id='link'>Bookings</Link></li>
-                        <Button className="auth" text="Login / Sign up" />
+                    <div>
+                        <ul>
+                            <li className="li-items"><Link to="/" id='link'>Home</Link></li>
+                            <li className="li-items"><Link to="/blog" id='link'>Blog</Link></li>
+                            <li className="li-items"><Link to="/aboutus" id='link'>About Us</Link></li>
+                            <li className="li-items"><Link to="/help" id='link'>Help</Link></li>
+                            <li className="li-items"><Link to="/bookings" id='link'>Bookings</Link></li>
+                        </ul>
+                    </div>
+                    <div className='auth-btn-container'>
+                        <Button className="auth-login" text="Login" />
+                        <Button className="auth-signup" text="Sign Up" />
                         <button className='menu-icon-btn' onClick={toggleMenu} >
                             <img className="menu-icon" src="/menu-b.png" alt="Hamburger-menu" />
                         </button>
-                    </ul>
+                    </div>
                 </nav>
-                    {menu && (
-                        <div className="menu">
-                            <img className='close-btn' src="/close-white.png" alt="close-icon" onClick={()=>{setMenu(false)}} />
-                            <li className="menu-items"><Link to="/" id='link'>Home</Link></li>
-                            <li className="menu-items"><Link to="/blog" id='link'>Blog</Link></li>
-                            <li className="menu-items"><Link to="/aboutus" id='link'>About Us</Link></li>
-                            <li className="menu-items"><Link to="/help" id='link'>Help</Link></li>
-                            <li className="menu-items"><Link to="/bookings" id='link'>Bookings</Link></li>
-                        </div>
-                    )}
+                {menu && (
+                    <div className="menu">
+                        <img className='close-btn' src="/close-white.png" alt="close-icon" onClick={() => { setMenu(false) }} />
+                        <li className="menu-items"><Link to="/" id='link'>Home</Link></li>
+                        <li className="menu-items"><Link to="/blog" id='link'>Blog</Link></li>
+                        <li className="menu-items"><Link to="/aboutus" id='link'>About Us</Link></li>
+                        <li className="menu-items"><Link to="/help" id='link'>Help</Link></li>
+                        <li className="menu-items"><Link to="/bookings" id='link'>Bookings</Link></li>
+                    </div>
+                )}
             </header>
         </>
     )
